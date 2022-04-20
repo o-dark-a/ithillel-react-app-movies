e0938573054fa8f78a69cd77dfb9e22ca8a02aa1
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-04-20 21:08:36 +0300

    1th task done

---
+++ homework_1/script.js
@@ -0,0 +1,8 @@
+// Task 1
+
+// Write a program that asks the user how old he is
+// and displays his age on the screen
+
+const userName = prompt(`What's your name?`, `Your name`);
+
+alert(`Hello, ${userName}! Nice to meet you :)`);
