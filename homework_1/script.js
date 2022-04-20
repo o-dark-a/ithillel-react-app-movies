df64375b3038f51543fa6a596c21df815948d115
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-04-20 21:23:38 +0300

    2nd task done

--- homework_1/script.js
+++ homework_1/script.js
@@ -3,6 +3,19 @@
 // Write a program that asks the user how old he is
 // and displays his age on the screen
 
-const userName = prompt(`What's your name?`, `Your name`);
+let userName = prompt(`What's your name?`, `Your name`);
 
 alert(`Hello, ${userName}! Nice to meet you :)`);
+
+// Task 2
+
+// Write a program that asks the user what is it name and where is it from (country, city)
+// and print the result to the console.
+
+let firstName = prompt(`What's your name?`, `Your name`);
+let userCountry = prompt(`Where are you from?`, `Enter your country`);
+let userCity = prompt(`Where are you from?`, `Enter your city`);
+
+alert(`Hello, your name is ${firstName}. You live in ${userCity}, ${userCountry}`);
+
+
