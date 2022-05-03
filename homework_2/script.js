4f0aa80fa66d7d8143e050f7a0c687f446efea99
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-05-03 13:55:43 +0100

    1th task is done

---
+++ homework_2/script.js
@@ -0,0 +1,15 @@
+// Task 1
+
+// Create three variables
+// Assign the first variable a numeric value
+// The second variable should be equal the first variable increased by 3 times
+// The third variable is equal to the sum of the first two
+// Print all three variables to the console.
+
+let numericVar = 9;
+let increasedVar = numericVar * 3;
+let sumVar = numericVar + increasedVar;
+
+console.log(numericVar);
+console.log(increasedVar);
+console.log(sumVar);
