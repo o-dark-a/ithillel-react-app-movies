270686db4de7e917c1b243071e8c7f4a5b424da6
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-05-23 12:32:12 +0100

    3rd task done

--- homework_8/script.js
+++ homework_8/script.js
@@ -2,7 +2,7 @@
 
 // Create a vegetable object with such properties that the following code returns "onions are usually white".
 
-const vegetable = {
+let vegetable = {
   name: 'onion', 
   color: 'white', 
 }
@@ -18,3 +18,23 @@ vegetable.shape = 'round';
 
 const { shape } = vegetable;
 console.log(`${name}s are usually ${shape}`);
+
+// Task 3
+
+// Create an array of vegetable objects (each element of the array must have the same structure as the vegetable
+// object) so that the following code returns "cucumbers are usually green", "tomatoes are usually red".
+
+const vegetables = [
+  {
+    name: 'cucumber', 
+    color: 'green',
+  },
+  {
+    name: 'tomato', 
+    color: 'red',
+  },
+]
+
+const [cucumber, tomato] = vegetables;
+console.log(`${cucumber.name}s are usually ${cucumber.color}`);
+console.log(`${tomato.name}s are usually ${tomato.color}`);
