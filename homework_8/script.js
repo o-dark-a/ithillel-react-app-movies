bcb7abdcd60d13265d0a35811465c0ad6e2329bc
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-05-23 12:02:44 +0100

    1st task done

---
+++ homework_8/script.js
@@ -0,0 +1,11 @@
+// Task 1
+
+// Create a vegetable object with such properties that the following code returns "onions are usually white".
+
+const vegetable = {
+  name: 'onion', 
+  color: 'white', 
+}
+
+const { name, color } = vegetable;
+console.log(`${name}s are usually ${color}`);
