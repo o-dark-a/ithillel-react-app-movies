b141c17181188e844d5554570db4115134a34731
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-05-28 16:47:08 +0100

    2nd task done

--- homework_10/script.js
+++ homework_10/script.js
@@ -26,3 +26,26 @@ buffer("A");
 buffer("XYZ");
 buffer(23);
 console.log(buffer()); // AXYZ
+
+// Task 2
+
+// Create a signUp() function that accepts userName, password parameters and returns an object with a userName
+// property and a signIn() method.
+// The signIn() method takes the newPassword parameter and compares its value with the value of password.
+
+// If the passwords match, the method returns the message:
+// 'Sign in success for ${userName}', otherwise - 'Password is incorrect'.
+
+// Create a user variable and store the result of calling the signUp() function into it.
+// Output the result of the function to the console.
+
+function signUp(userName, password) {
+  return {
+    userName,
+    signIn: (newPassword) => (newPassword === password) ? `Sign in success for ${userName}` : 'Password is incorrect',
+  }
+}
+
+let user = signUp("billy", "qwerty");
+console.log(user.signIn("a")); // Password is incorrect
+console.log(user.signIn("qwerty")); // Sign in success for billy
