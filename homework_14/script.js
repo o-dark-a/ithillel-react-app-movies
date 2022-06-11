cd50b3508f0d6e848e4d3806bc710e215f62bcf3
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-06-11 17:49:17 +0100

    3rd task done

--- homework_14/script.js
+++ homework_14/script.js
@@ -41,3 +41,63 @@ function addImage() {
 }
 
 addImage();
+
+// Task 3
+
+// Ask the user for a number and add <input> tags to the form (before the Register button) equal to that number.
+
+// Requirements for the <input> tag:
+//  - each input must contain the 'input-item' class, value = `Input ${index}`;
+//  - the last input must have an additional 'margin-zero' class;
+//  - create your own class with background-color property and add it to all odd inputs;
+//  - clear the value of every third input and give it a placeholder attribute with any text.
+
+function createNewInput() {
+  const inputCount = +prompt('How many inputs would you like to add?');
+  const isValidCount = inputCount > 0 && Number.isInteger(inputCount);
+
+  if (isValidCount) {
+    const form = document.querySelector('form');
+
+    for (let i = inputCount; i > 0; i--) {
+      const input = document.createElement('input');
+      input.classList.add('input-item');
+      input.value = `Input ${i}`;
+      form.prepend(input);
+    }
+  }
+}
+
+createNewInput();
+
+// the last input must have an additional 'margin-zero' class
+function highlightLastInput() {
+  document.querySelector('.input-item:last-of-type').classList.add('margin-zero');
+}
+
+highlightLastInput();
+
+// create your own class with background-color property and add it to all odd inputs
+function highlightOddInputs() {
+  const allInputs = document.querySelectorAll('.input-item');
+  for (let i = 0; i < allInputs.length; i++) {
+    if((i+1) % 2) {
+      allInputs[i].classList.add('odd-inputs');
+    }
+  }
+}
+
+highlightOddInputs();
+
+// clear the value of every third input and give it a placeholder attribute with any text
+function clearEveryThirdElem() {
+  const allInputs = document.querySelectorAll('.input-item');
+  for (let i = 0; i < allInputs.length; i++) {
+    if(!((i+1) % 3)) {
+      allInputs[i].value = '';
+      allInputs[i].placeholder = 'Some text';
+    }
+  }
+}
+
+clearEveryThirdElem();
