bd6d1cd231d5c427678422552c31f389c95c4c92
Daria Vozdraganova <dariweb.mail@gmail.com>
2022-06-11 15:56:09 +0100

    2nd task done

--- homework_14/script.js
+++ homework_14/script.js
@@ -17,3 +17,27 @@ function swappingBlocks() {
 }
 
 swappingBlocks();
+
+// Task 2
+
+// Write a function that asks the user for permission to add an image (confirm()) and, if he agrees,
+// adds the image to the page (the user must set the link to the image himself).
+
+function addImage() {
+  if(confirm('Would you like to add some image?')) {
+
+    const defaultImgLink = 'https://images.unsplash.com/photo-1593483316242-efb5420596ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG92ZWx5JTIwY2F0fGVufDB8fDB8fA%3D%3D&w=1000&q=80';
+    const userImgLink = prompt('Insert image link', defaultImgLink);
+    const currentImgLink = userImgLink ? userImgLink : defaultImgLink;
+
+    const wrapper = document.querySelector('#wrapper');
+    const image = document.createElement('img');
+    image.src = currentImgLink;
+    image.alt = 'image description';
+
+    wrapper.append(image);
+    
+  }
+}
+
+addImage();
